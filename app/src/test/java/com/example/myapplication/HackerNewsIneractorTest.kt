654f6bb61c2af151d21a4646d03550d03b1146e6
Sierra Obryan <sierra@atomicrobot.com>
2019-09-18 11:17:37 -0400

    Fix test

--- app/src/test/java/com/example/myapplication/HackerNewsIneractorTest.kt
+++ app/src/test/java/com/example/myapplication/HackerNewsIneractorTest.kt
@@ -3,9 +3,11 @@ package com.example.myapplication
 import android.app.Application
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.myapplication.data.model.Item
 import com.example.myapplication.data.room.ItemStore
 import com.example.myapplication.data.network.HackerNewsApiService
 import com.example.myapplication.data.network.HackerNewsInteractor
+import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.Single
 import io.reactivex.observers.TestObserver
@@ -40,12 +42,11 @@ class HackerNewsIneractorTest {
     @Test
     @Throws(Exception::class)
     fun testLoadStories() {
-//        val mockResponse = Single.just(Response.success(
-////            listOf(
-////            Item(1, false, "story", "name", 1, false, listOf(), 2, "", "", "", listOf(), 1, 1))
-////        ))
         val mockResponse = Single.just(Response.success(listOf(1)))
+        val mockItemResponse = Single.just(Response.success(Item(1, false, "story", "name", 1, false, listOf(1), 2, "", "", "", listOf(1), 1, 1)
+        ))
         whenever(api.getTopStories()).thenReturn(mockResponse)
+        whenever(api.getItem(any())).thenReturn(mockItemResponse)
 
         val subscriber = TestObserver<HackerNewsInteractor.LoadStoriesResponse>()
         interactor.loadStories(HackerNewsInteractor.LoadStoriesRequest()).subscribeWith(subscriber)
@@ -56,6 +57,8 @@ class HackerNewsIneractorTest {
         subscriber.assertComplete()
 
         val response = subscriber.values()[0]
-        assertEquals(1, response)
+        assertEquals(1, response.items.size)
+        assertEquals(Item(1, false, "story", "name", 1, false, listOf(1), 2, "", "", "", listOf(1), 1, 1)
+        , response.items[0])
     }
 }
